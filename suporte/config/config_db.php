ae4ac2f87edf82e530d6bd3edeb129fb1d911591
sebastiaosneto <neto2090@hotmail.com>
2026-05-02 12:30:27 -0300

    adicionando sistema de suporte

---
+++ suporte/config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = 'localhost';
+   public $dbuser     = 'u647286339_glpi';
+   public $dbpassword = 'M%40ximus9249%40';
+   public $dbdefault  = 'u647286339_glpi';
+}
